4fb47935aa4a5f8a78da1ab177d9268327ea580b
ok-lab <60685175+ceo21ckim@users.noreply.github.com>
2022-09-01 00:10:18 +0900

    Delete basic_tasks directory

--- basic_tasks/settings.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-BASE_DIR = os.path.dirname(__file__)
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-
-NODE_DIR = os.path.join(DATA_DIR, 'nodes.csv')
-EDGE_DIR = os.path.join(DATA_DIR, 'edges.csv')
